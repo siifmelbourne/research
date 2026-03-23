07d8bd0458e39eff9625e9182d993c30e3812546
flatplum <flatplum@proton.me>
2026-03-23 17:33:03 +1100

    feat: initial commit on main

---
+++ app/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <iframe
+        src="https://appfinancialreport-eporheljnxauzaq2e3yr4e.streamlit.app//?embed=true" 
+        width="100%"
+        height="600"
+        style="border:none;">
+    </iframe>
+    <iframe
+        src="https://siif-dashboard.streamlit.app///?embed=true" 
+        width="100%"
+        height="600"
+        style="border:none;">
+    </iframe>
+  </div>
+</template>
